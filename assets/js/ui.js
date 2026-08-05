cf9754a2f4ec06b5d422ff7f1bebd2bcf3686382
nasim-orion <nasimch2709@gmail.com>
2026-08-05 10:28:34 +0100

    rudimentary quiz

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,3 +1,25 @@
 // UI related JS
 
 // Start button
+
+
+
+
+
+
+
+// quiz
+
+
+function startQuiz() {
+  const main = document.getElementById('quiz-content');
+
+  main.innerHTML = `
+    <section class="quiz-card">
+      <h2>Question 1</h2>
+      <p>Choose an answer, how would you respond?</p>
+      <button>Option A</button>
+      <button>Option B</button>
+    </section>
+  `;
+}document.addEventListener('DOMContentLoaded', renderIntro);
