96f83bb6eb86b1d94f3d9c625abdacb3035950db
molly-thompson <mollyerinn@gmail.com>
2026-08-09 22:03:15 +0100

    Configure application routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,14 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+	index,
+	layout,
+	type RouteConfig,
+	route,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+	layout("layouts/app-layout.tsx", [
+		index("routes/home.tsx"),
+		route("assessment", "routes/assessment.tsx"),
+		route("results", "routes/results.tsx"),
+	]),
+] satisfies RouteConfig;
